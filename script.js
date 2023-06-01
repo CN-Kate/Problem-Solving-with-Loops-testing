7b4bb8753c7e6aa6142169210e89b956686ea76f
kate-cronin <97194571+kate-cronin@users.noreply.github.com>
2023-06-01 13:01:20 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,80 @@
+//PUZZLE 1: Replace the ??? in the code below to correctly to print the string "Success" to the console. 
+
+let num2 = 149;
+if (num2 >= 150) {
+  console.log("Nope");
+} else if (num2 <= 148) {
+  console.log("Still no");
+} else {
+  console.log("Success");
+}
+
+
+//PUZZLE 2: Replace the ??? in the code below so that each number in the array list1 is printed to the console once. 
+
+let list1 = [5, 6, 11, 3];
+for (let mystery of list1) {
+  console.log(mystery);
+}
+
+//PUZZLE 3: Replace the ??? in the code below so that the numbers 11, 21, and 35 are each printed to the console once.
+
+// let list2 = [???];
+// for (let originalNumber of list2) {
+//   console.log(originalNumber);
+// }
+
+
+//PUZZLE 4: Replace the ??? in the code below so that the numbers 11, 21, and 35 are each printed to the console once.
+
+// let list3 = [???];
+// for (let originalNumber of list3) {
+//   console.log(originalNumber + 3);
+// }
+
+//PUZZLE 5: Replace the ??? in the code below so the console says: 
+// Printing: 4
+// Printing: 5
+// Printing: 6
+// Printing: 7
+// Printing: 8
+
+// let list4 = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// for (let number of list4) {
+//   if (???) {
+//     console.log("Printing: " + number);
+//   }
+// }
+
+
+//PUZZLE 6: Write an if statement that adds 10 to a number if it’s less than 10 and subtracts 10 from all other numbers. For example, if myNum is equal to 4, then the console should show 14. If myNum is equal to 100, then 90 should be printed to the console. 
+
+// let myNum = 4;
+
+//WRITE YOUR CODE HERE
+
+//PUZZLE 7: Write a for of loop that logs each of the fruits in the fruits array below to the console. 
+
+//let fruits = ["apple", "orange", "banana"];
+
+//WRITE YOUR CODE HERE
+
+
+
+//PUZZLE 8 (SPICY): You will need to combine your knowledge of conditionals, arrays, and loops to solve this puzzle. Write the code that will log all of the numbers greater than 50 OR less than 30 to the console. 
+
+//let bigList = [81, 47, 66, 15, 11, 25, 45, 5, 99, 85, 58, 46, 21, 92, 38, 84, 66, 93, 65, 61, 7, 85, 2, 74, 66, 88, 65, 48, 58, 87, 41, 56, 17, 46, 26, 87, 41, 69, 48, 34, 69, 3, 46, 65, 67, 76, 46, 35, 68, 86];
+
+//WRITE YOUR CODE HERE
+
+
+
+
+
+
+
+
+
+
+
